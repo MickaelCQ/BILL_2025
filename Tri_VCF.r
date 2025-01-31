36a07ebd425ade0e191474919e6e8d6b66766ad3
Mickael <coquerelle.m30@gmail.com>
2025-01-31 17:26:42 +0100

    Ajout du formatage nom file et d'une regex pour le nom de groupe et du réplicat pour discriminer les SV TODO, indexer dans une colonne cryptique cette variable comme pour MU_COVERAGE ou Genfile

--- Tri_VCF.r
+++ Tri_VCF.r
@@ -1,20 +1,20 @@
 #01/29/25 Structuring and assembly of VCF files listing structural variants of interest
 #01/30/25 Programming a function for read and extract columns natives of the VCFs and generate a csv
 #01/30/25 Reflection and programming a function for the addition of INFO column metadata
-#01/30/25 GIT update, comment translation and test set on generation P15, P30, P50 and P65
-#01/31/25 Add a cryptic column on first index with the willingness to identify for each SV the viral generation
+#01/30/25 GIT update, comment translation and test set on genFile P15, P30, P50 and P65
+#01/31/25 Add a cryptic column on first index with the willingness to identify for each SV the viral genFile
 
 library(vcfR) # no std library R for import function for the VCF treatment
 
 ################################# 
-# Generic Functions
+# GENERIC FUNCTION
 ##################################
 
 # For read and extract data in the VCF file
 FunExtractVCF <- function(vcf_file) {
   vcf <- vcfR::read.vcfR(vcf_file)  # Read this file
   
-  # Extraire les colonnes fixes du fichier VCF (comme CHROM, POS, etc.)
+  # Extract all column in VCF in dataframe (except INFO)
   nativesCol <- as.data.frame(vcf@fix)  # Extract 
   
   ################################# 
@@ -22,110 +22,131 @@ FunExtractVCF <- function(vcf_file) {
   ##################################
   
   FunExtractINFO <- function(info_col) {
-    # Séparer les informations de chaque ligne de la colonne INFO en fonction du séparateur ";"
+    #separate the information for each line of each INFO column using the separator “;”.
     infos <- strsplit(info_col, ";")
     
     # Identifier les noms de champs uniques dans toutes les informations de la colonne INFO
-    champs_possibles <- unique(unlist(lapply(infos, function(x) {
-      sapply(strsplit(x, "="), function(y) y[1])
-    })))
+    allFields <- unique(unlist
+                        (lapply
+                          (infos, function(x) {sapply(strsplit(x, "="), function(y) y[1])}
+                          )#lapply end
+                        )#lunlist end
+                      )#unique end
     
-    # Crée une liste où chaque élément représente une ligne de la colonne INFO avec les valeurs associées aux champs
+    # Create a list for each element represente one line of the column INFO with the values associates with the fields
     infos_list <- lapply(infos, function(x) {
       pairs <- strsplit(x, "=")
-      values <- sapply(pairs, function(y) ifelse(length(y) > 1, y[2], y[1]))  # Extrait la valeur ou la clé
-      names(values) <- sapply(pairs, function(y) y[1])  # Associe les noms aux valeurs
       
-      # Remplir les champs manquants avec NA
-      complet <- setNames(rep(NA, length(champs_possibles)), champs_possibles)
-      complet[names(values)] <- values
-      return(complet)
-    })
+      # Extract the key or the value
+      values <- sapply(pairs, function(y) ifelse(
+        length(y) > 1, y[2], y[1]
+                                                ) #end ifelse
+                      )#end sapply
+      
+      #Associated names with the values :
+      names(values) <- sapply(pairs, function(y) y[1]) 
+      
+      # Fill in missing fields with NA:
+      full <- setNames(rep(NA, length(allFields)), allFields)
+      full[names(values)] <- values
+      return(full)
+    } #end function x
     
-    # Combine tous les éléments de la liste en un seul DataFrame
+    )#end lapply
+    
+    # Merge all list items into a single dataframe:
     infos_df <- do.call(rbind, infos_list)
-    infos_df <- as.data.frame(infos_df, stringsAsFactors = FALSE)  # Assure que les colonnes sont traitées comme des chaînes
+    infos_df <- as.data.frame(infos_df, stringsAsFactors = FALSE)  # Check for each column is treated as a string
     
     return(infos_df)
   }
   
-  # Appliquer la fonction sur la colonne INFO pour extraire les données pertinentes
-  info_colonnes <- FunExtractINFO(nativesCol$INFO)
+  # Apply function to extract information of the column INFO:
+  infoColumns <- FunExtractINFO(nativesCol$INFO)
   
-  ##################################################################################
-  # AJOUT D'UNE COLONNE POUR LA MOYENNE DES PROFONDEURS DE RÉPLICATS (COVERAGE)
-  ##################################################################################
+  #######################################################
+  # ADD A CRYCTIC COLUMN FOR AVERAGE DEPTH FOR EACH SV 
+  #######################################################
   calcMoyCoverage <- function(coverage) {
-    if (!is.na(coverage) && nzchar(coverage)) {
-      val_prof <- as.numeric(unlist(strsplit(coverage, ",")))
+    if (!is.na(coverage) && nzchar(coverage)) { # If value in cell and not empty
+      val_prof <- as.numeric(unlist(strsplit(coverage, ","))) # strip ",", merge in vector, convert this items in numeric values
       if (length(val_prof) > 0) {
-        return(mean(val_prof, na.rm = TRUE))
+        return(mean(val_prof, na.rm = TRUE)) # ignore NA and calc mean
       }
     }
-    return(NA)
+    return(NA) # implicit "else" to avoir empty cells
   }
   
-  if ("COVERAGE" %in% names(info_colonnes)) {
-    info_colonnes$MU_COVERAGE <- sapply(info_colonnes$COVERAGE, calcMoyCoverage)
+  if ("COVERAGE" %in% names(infoColumns)) { # Conditional check existence for current SV of "COVERAGE" from the futur calc mean :
+    infoColumns$MU_COVERAGE <- sapply(infoColumns$COVERAGE, calcMoyCoverage)
   } else {
-    info_colonnes$MU_COVERAGE <- NA
+    infoColumns$MU_COVERAGE <- NA
   }
   
   colonnes_reorganisees <- c(
-    setdiff(names(info_colonnes), c("COVERAGE", "MU_COVERAGE")),
+    setdiff(names(infoColumns), c("COVERAGE", "MU_COVERAGE")),
     "COVERAGE",
     "MU_COVERAGE"
   )
-  info_colonnes <- info_colonnes[, colonnes_reorganisees, drop = FALSE]  # Évite les erreurs si colonnes manquent
+  infoColumns <- infoColumns[, colonnes_reorganisees, drop = FALSE]  # Avoids columns if columns are missing
   
-  ##################################################################################
-  # COSMÉTIQUE DU CSV
-  ##################################################################################
-  donnees_finales <- cbind(
+  ####################
+  # CSV Cosmetics
+  ####################
+  finalData <- cbind(
     nativesCol[, c("CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER")],
-    info_colonnes
+    infoColumns
   )
   
-  # Extraire la génération du nom du fichier
-  generation <- gsub("^P([0-9]+)-.*$", "P\\1", basename(vcf_file))
-  donnees_finales$GENERATION <- generation
-  
-  return(donnees_finales)
+  #####################################################
+  # A little Regex .....
+  ####################################################
+  # Extract relevance identification from the VCF file name (for classifying the structural variant)
+  genFile <- gsub("^P([0-9]+)-.*$", "P\\1", basename(vcf_file))
+  groupFile <- sub("^([^\\.]+\\.[^\\.]+).*", "\\1", basename(vcf_file))
+
+  finalData$genFile <- genFile
+  return(finalData)
 }
 
-##################################################################################
-# TRAITEMENT DE PLUSIEURS FICHIERS VCF AVEC CHAMPS VARIABLES DANS INFO
-##################################################################################
+#############################################
+# successive treatments of VCFs of interest
+#############################################
 vcf_files <- list.files(path = "/home/mickael/Projets_GIT/BILL_2025/VCF/", pattern = "*.vcf", full.names = TRUE)
-toutes_les_donnees <- NULL  # Initialiser comme NULL pour éviter les conflits
+allData <- NULL  # Initialize as NULL to avoid conflicts at runtime
 
+# Final LOOP
 for (vcf_file in vcf_files) {
-  donnees_vcf <- FunExtractVCF(vcf_file)  # Extraire les données du fichier VCF
+  vcfDATA <- FunExtractVCF(vcf_file)  # Extraire les données du fichier VCF
   
-  if (is.null(toutes_les_donnees)) {
-    # Si c'est le premier fichier traité, initialiser avec ses données
-    toutes_les_donnees <- donnees_vcf
+  if (is.null(allData)) {
+    # If the First file processed , initialize with its data 
+    allData <- vcfDATA
   } else {
-    # Trouver toutes les colonnes existantes entre les données accumulées et les nouvelles données
-    colonnes_toutes <- unique(c(names(toutes_les_donnees), names(donnees_vcf)))
     
-    # Ajouter des colonnes manquantes à toutes_les_donnees
-    for (col in setdiff(colonnes_toutes, names(toutes_les_donnees))) {
-      toutes_les_donnees[[col]] <- NA
+    # Find all existing columns and accumulated data :
+    allColumns <- unique(c(names(allData), names(vcfDATA)))
+    
+    # Add missing collumns at "allData"
+    for (col in setdiff(allColumns, names(allData))) {
+      allData[[col]] <- NA
     }
     
-    # Ajouter des colonnes manquantes à donnees_vcf
-    for (col in setdiff(colonnes_toutes, names(donnees_vcf))) {
-      donnees_vcf[[col]] <- NA
+    # Add missing columns at "vcfDATA"
+    for (col in setdiff(allColumns, names(vcfDATA))) {
+      vcfDATA[[col]] <- NA
     }
     
-    # Réordonner les colonnes et fusionner
-    toutes_les_donnees <- rbind(
-      toutes_les_donnees[, colonnes_toutes, drop = FALSE],
-      donnees_vcf[, colonnes_toutes, drop = FALSE]
-    )
-  }
-}
+    #Reorder columns and merge
+    allData <- rbind(
+      allData[, allColumns, drop = FALSE],
+      vcfDATA[, allColumns, drop = FALSE]
+    )#end rbind
+  }#end else
+}#end loop
 
-# Exporter les données dans un fichier CSV
-write.csv(toutes_les_donnees, "/home/mickael/Projets_GIT/BILL_2025/Traitement_VCF.csv", row.names = FALSE)
+# Export DATA in file csv with current date
+fileName <- paste0("/home/mickael/Projets_GIT/BILL_2025/Traitement_VCF_", Sys.Date(), ".csv")
+write.csv(allData, fileName, row.names = FALSE)
+
+
